e8274956e370776afafda581a5e43513e808b7ef
Mauricio Vargas <vargasmauricio65@gmail.com>
2026-01-29 20:21:02 -0400

    feat: export engine contents

---
+++ packages/core/src/engine/index.ts
@@ -0,0 +1,11 @@
+export { Engine } from './engine';
+export { HandlerRegistry } from './handlerRegistry';
+export { PreconditionChecker } from './preconditionChecker';
+export { HandlerExecutor } from './handlerExecutor';
+export { ResultFormatter } from './resultFormatter';
+export { ManifestResolver } from './manifestResolver';
+export { ParameterValidator } from './validation/parameterValidator';
+export { StrategyValidator } from './validation/strategyValidator';
+
+export type { CapabilityGraph } from './engine';
+export type { HandlerExecutionResult } from './handlerExecutor';
