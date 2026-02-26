6fbabd5066509ec5891dd42cdc8e2047319bb946
Mauricio Vargas <vargasmauricio65@gmail.com>
2026-02-26 16:47:26 -0400

    fix: added required exports

--- packages/core/src/engine/index.ts
+++ packages/core/src/engine/index.ts
@@ -4,8 +4,10 @@ export { PreconditionChecker } from './preconditionChecker';
 export { HandlerExecutor } from './handlerExecutor';
 export { ResultFormatter } from './resultFormatter';
 export { ManifestResolver } from './manifestResolver';
+export { StrategyExecutor } from './strategyExecutor';
 export { ParameterValidator } from './validation/parameterValidator';
 export { StrategyValidator } from './validation/strategyValidator';
 
 export type { CapabilityGraph } from './engine';
 export type { HandlerExecutionResult } from './handlerExecutor';
+export * from './types';
